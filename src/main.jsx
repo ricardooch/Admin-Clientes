be4eb4c862d7cf32ede5d377f846ef72735a5298
Ricardo <ricardo8arr@gmail.com>
2022-12-01 21:39:38 -0600

    agregar y editar clientes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
-import NuevoCliente from './pages/NuevoCliente'
-import Index from './pages/Index'
+import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
+import Index, {loader as clientesLoader } from './pages/Index'
+import EditarCliente, { loader as editarClienteLoader, action as editarClienteAction } from './pages/EditarCliente'
+import ErrorPage from './components/ErrorPage'
+import { action as eliminarClienteAction } from './components/Cliente'
 
 const router = createBrowserRouter([
   {
@@ -13,11 +16,27 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />
+        element: <Index />,
+        loader: clientesLoader,
+        errorElement: <ErrorPage />
       },
       {
         path: '/clientes/nuevo',
-        element: <NuevoCliente />
+        element: <NuevoCliente />,
+        action: nuevoClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editarClienteLoader,
+        action: editarClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction,
+        errorElement: <ErrorPage /> 
       }
     ]
   },
